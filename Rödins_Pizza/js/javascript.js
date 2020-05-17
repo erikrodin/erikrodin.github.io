796e1ef7a71b5aaeb305826dc33c9100852a52db
erikrodin <45962044+erikrodin@users.noreply.github.com>
2020-05-17 13:40:02 +0200

    klar för test

--- Rödins_Pizza/js/javascript.js
+++ Rödins_Pizza/js/javascript.js
@@ -20,7 +20,7 @@ $("#kostnad").show()
 $("#rubriker3-2").hide();
 $(".mobil-2").hide();
 $(".mobilknapp-2").hide();
-$(".bestpizz").hide();
+$("#bestpizz").hide();
 $("#stjärna1").show();
 $("#stjärna2").show();
 $("#stjärna3").show();
@@ -128,7 +128,7 @@ document.getElementById("window").style.overflow = "hidden";
 $(document).ready(function (){
 
 
-  document.getElementById("dot1").style.backgroundColor = "lightblue"; //använder javascript för att sätta ljusblå bakgrundsfärg på dot1 när slidern öppnas
+  document.getElementById("dot1").style.backgroundColor = "brown"; //använder javascript för att sätta ljusblå bakgrundsfärg på dot1 när slidern öppnas
 
 
 });
@@ -148,20 +148,20 @@ $("#höger").click(function(){
     if (plats==0) {
         slider.animate({right:"408"})  //om "plats" =0 så kommer scriptet att flytta på sig 408px från höger => bild 3 visas
         plats=2;     //samt att "plats kommer att tilldelas värdet 2"
-        document.getElementById("dot3").style.backgroundColor = "lightblue"; //dot3 blir ljusblå när man befinner sig på sista bilden, vilket hjälper till att navigera vart man är i slidern
+        document.getElementById("dot3").style.backgroundColor = "brown"; //dot3 blir ljusblå när man befinner sig på sista bilden, vilket hjälper till att navigera vart man är i slidern
         document.getElementById("dot2").style.backgroundColor = "white"; //dot2 blir vit för att visa att man inte befinner sig på bild 2 i slidern
         document.getElementById("dot1").style.backgroundColor = "white"; //dot1 blir vit för att visa att man inte befinner sig på bild 2 1 slidern
     }else if (plats==1) {
         slider.animate({right:"0"})   //om plats =1 så återgår scriptet till position 0px => bild 1 visas
         plats=0;    //samt att "plats" kommer tilldelas värdet 0 efter att satsen exikverats
         // vilket leder till att slidern återgår till bild 1 efter exikveringen
-        document.getElementById("dot1").style.backgroundColor = "lightblue"; //dot1 blir ljusblå när man befinner sig på första bilden, vilket hjälper till att navigera vart man är i slidern
+        document.getElementById("dot1").style.backgroundColor = "brown"; //dot1 blir ljusblå när man befinner sig på första bilden, vilket hjälper till att navigera vart man är i slidern
         document.getElementById("dot2").style.backgroundColor = "white"; //dot2 blir vit för att visa att man inte befinner sig på bild 2 i slidern
         document.getElementById("dot3").style.backgroundColor = "white"; //dot3 blir vit för att visa att man inte befinner sig på bild 3 i slidern
     }else if (plats==2) {
         slider.animate({right:"204"})   //om "plats"=2 så visar scriptet från 204px och framåt => bild 2 visas
         plats=1; //efter exikveringen tilldelas "plats" värdet 1
-        document.getElementById("dot2").style.backgroundColor = "lightblue"; //dot2 blir ljusblå när man befinner sig på andra bilden, vilket hjälper till att navigera vart man är i slidern
+        document.getElementById("dot2").style.backgroundColor = "brown"; //dot2 blir ljusblå när man befinner sig på andra bilden, vilket hjälper till att navigera vart man är i slidern
         document.getElementById("dot3").style.backgroundColor = "white"; //dot3 blir vit för att visa att man inte befinner sig på bild 3 i slidern
         document.getElementById("dot1").style.backgroundColor = "white"; //dot1 blir vit för att visa att man inte befinner sig på bild 1 i slidern
     }
@@ -183,14 +183,14 @@ $("#vänster").click(function(){
     if (plats==0) {
         slider.animate({right:"+=204"}) //om platsen är noll så kommer scriptet att visa de första 0 - 204 pixlarna => bild 1 visas
         plats++;                              // samt att variabeln "plats" kommer öka med +1 efter att satsen exikverats
-        document.getElementById("dot2").style.backgroundColor = "lightblue"; //dot2 blir ljusblå när man befinner sig på andra bilden, vilket hjälper till att navigera vart man är i slidern
+        document.getElementById("dot2").style.backgroundColor = "brown"; //dot2 blir ljusblå när man befinner sig på andra bilden, vilket hjälper till att navigera vart man är i slidern
         document.getElementById("dot3").style.backgroundColor = "white"; //dot3 blir vit för att visa att man inte befinner sig på bild 3 i slidern
         document.getElementById("dot1").style.backgroundColor = "white"; //dot1 blir vit för att visa att man inte befinner sig på bild 1 i slidern
 
     }else if (plats==1) {
         slider.animate({right:"+=204"})  //om platsen är ett så kommer scriptet att visa pixlarna 204 - 608  => bild 2 visas
         plats++;    // samt att variabeln "plats" kommer öka med +1 efter att satsen exikverats
-        document.getElementById("dot3").style.backgroundColor = "lightblue"; //dot3 blir ljusblå när man befinner sig på tredje bilden, vilket hjälper till att navigera vart man är i slidern
+        document.getElementById("dot3").style.backgroundColor = "brown"; //dot3 blir ljusblå när man befinner sig på tredje bilden, vilket hjälper till att navigera vart man är i slidern
         document.getElementById("dot2").style.backgroundColor = "white"; //dot2 blir vit för att visa att man inte befinner sig på bild 2 i slidern
         document.getElementById("dot1").style.backgroundColor = "white"; //dot1 blir vit för att visa att man inte befinner sig på bild 1 i slidern
 
@@ -198,7 +198,7 @@ $("#vänster").click(function(){
           slider.animate({right:"0"})   //om platsen är noll så kommer scriptet att visa pixlarna 608 - 812 => bild 3 visas
           plats=0;  // samt att variabeln "plats" kommer tilldelas värdet 0 efter att satsen exikverats
                     // vilket leder till att slidern återgår till bild 1 efter exikveringen
-          document.getElementById("dot1").style.backgroundColor = "lightblue"; //dot1 blir ljusblå när man befinner sig på första bilden, vilket hjälper till att navigera vart man är i slidern
+          document.getElementById("dot1").style.backgroundColor = "brown"; //dot1 blir ljusblå när man befinner sig på första bilden, vilket hjälper till att navigera vart man är i slidern
           document.getElementById("dot2").style.backgroundColor = "white"; //dot2 blir vit för att visa att man inte befinner sig på bild 2 i slidern
           document.getElementById("dot3").style.backgroundColor = "white"; //dot3 blir vit för att visa att man inte befinner sig på bild 3 i slidern
     }
@@ -221,14 +221,14 @@ $(document).ready(function(){ //gör så att koden kommer exikveras när hela do
           if (plats==0) {
             slider.animate({right:"+=204"}) //om platsen är noll så kommer scriptet att visa de första 0 - 204 pixlarna => bild 1 visas
             plats++;                              // samt att variabeln "plats" kommer öka med +1 efter att satsen exikverats
-          document.getElementById("dot2").style.backgroundColor = "lightblue"; //dot2 blir ljusblå när man befinner sig på andra bilden, vilket hjälper till att navigera vart man är i slidern
+          document.getElementById("dot2").style.backgroundColor = "brown"; //dot2 blir ljusblå när man befinner sig på andra bilden, vilket hjälper till att navigera vart man är i slidern
           document.getElementById("dot3").style.backgroundColor = "white"; //dot3 blir vit för att visa att man inte befinner sig på bild 3 i slidern
           document.getElementById("dot1").style.backgroundColor = "white"; //dot1 blir vit för att visa att man inte befinner sig på bild 1 i slidern
 
           }else if (plats==1) {
             slider.animate({right:"+=204"})  //om platsen är ett så kommer scriptet att visa pixlarna 204 - 608  => bild 2 visas
             plats++;    // samt att variabeln "plats" kommer öka med +1 efter att satsen exikverats
-          document.getElementById("dot3").style.backgroundColor = "lightblue"; //dot3 blir ljusblå när man befinner sig på tredje bilden, vilket hjälper till att navigera vart man är i slidern
+          document.getElementById("dot3").style.backgroundColor = "brown"; //dot3 blir ljusblå när man befinner sig på tredje bilden, vilket hjälper till att navigera vart man är i slidern
           document.getElementById("dot2").style.backgroundColor = "white"; //dot2 blir vit för att visa att man inte befinner sig på bild 2 i slidern
           document.getElementById("dot1").style.backgroundColor = "white"; //dot1 blir vit för att visa att man inte befinner sig på bild 1 i slidern
 
@@ -236,7 +236,7 @@ $(document).ready(function(){ //gör så att koden kommer exikveras när hela do
             slider.animate({right:"0"})   //om platsen är noll så kommer scriptet att visa pixlarna 608 - 812 => bild 3 visas
               plats=0;  // samt att variabeln "plats" kommer tilldelas värdet 0 efter att satsen exikverats
               // vilket leder till att slidern återgår till bild 1 efter exikveringen
-          document.getElementById("dot1").style.backgroundColor = "lightblue"; //dot1 blir ljusblå när man befinner sig på första bilden, vilket hjälper till att navigera vart man är i slidern
+          document.getElementById("dot1").style.backgroundColor = "brown"; //dot1 blir ljusblå när man befinner sig på första bilden, vilket hjälper till att navigera vart man är i slidern
           document.getElementById("dot2").style.backgroundColor = "white"; //dot2 blir vit för att visa att man inte befinner sig på bild 2 i slidern
           document.getElementById("dot3").style.backgroundColor = "white"; //dot3 blir vit för att visa att man inte befinner sig på bild 3 i slidern
           }//koden gällande allt i if satserna listade jag ut själv
